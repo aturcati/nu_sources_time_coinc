b3d847c0656c23c9749d2757af7108f7f89a9219
aturcati <andrea.turcati@tum.de>
2020-07-02 16:18:03 +0200

    Update 2.07

--- lib/functions.py
+++ lib/functions.py
@@ -4,42 +4,70 @@ from tqdm import tqdm
 
 from .lightcurve import LightCurve
 
+
 def make_rnd_lc(bins, state, thr):
     rand_idx = np.random.choice(len(state), len(state))
     s_rand = LightCurve(bins, state[rand_idx[:-1]], thr)
     return s_rand
 
-def background_trials(ntrials, sources, bins, seed=1):
+
+def background_trials(ntrials, sources, seed=1):
     np.random.seed(seed)
     trials = []
     for i in tqdm(range(ntrials)):
-        t_nu = np.random.uniform(low=np.min(bins), high=np.max(bins), size=(len(sources),1))
-        coincidences = np.array([s(t) for s,t in zip(sources, t_nu)]).flatten()
+        t_nu = []
+        for i, s in enumerate(sources):
+            t_nu.append(
+                np.random.uniform(
+                    low=np.min(s._bins), high=np.max(s._bins), size=1
+                )
+            )
+        t_nu = np.array(t_nu)
+        coincidences = np.array(
+            [s(t) for s, t in zip(sources, t_nu)]
+        ).flatten()
         trials.append(np.sum(coincidences))
     return np.array(trials)
 
-def signal_trials(ntrials, n_inj, sources, bins, seed=1):
+
+def signal_trials(ntrials, n_inj_mean, sources, seed=1):
     np.random.seed(seed)
-    n_bkg = len(sources) - n_inj
     trials = []
     for i in tqdm(range(ntrials)):
-        t_nu = np.random.uniform(low=np.min(bins), high=np.max(bins), size=(n_bkg,1))
+        n_inj = np.random.poisson(n_inj_mean)
+        if n_inj > len(sources):
+            n_inj = len(sources)
+        n_bkg = len(sources) - n_inj
         rnd_idx = np.random.choice(len(sources), n_bkg, replace=False)
-        coincidences = np.array([s(t) for s,t in zip(sources[rnd_idx], t_nu)]).flatten()
+        t_nu = []
+        for i, s in enumerate(sources[rnd_idx]):
+            t_nu.append(
+                np.random.uniform(
+                    low=np.min(s._bins), high=np.max(s._bins), size=1
+                )
+            )
+        t_nu = np.array(t_nu)
+        coincidences = np.array(
+            [s(t) for s, t in zip(sources[rnd_idx], t_nu)]
+        ).flatten()
         trials.append(np.sum(coincidences) + n_inj)
     return np.array(trials)
+
 
 def test_statistic(n, pdf):
     p_zero = pdf[0, n]
     p_h = np.max(pdf[1:, n])
     ind = np.argmax(pdf[1:, n])
-    return -2 * np.log(p_zero/p_h) if p_h !=0. else 0., ind
+    return -2 * np.log(p_zero / p_h) if p_h != 0.0 else 0.0, ind
+
 
 def prof_likelihood(n, pdf):
-    p_zero = pdf[0, n]
+    p_zero = np.array(pdf[0, n])
+    p_h = np.array(pdf[1:, n])
+    m = p_h != 0.0
+    p_h = p_h[m]
     p_h_max = np.max(pdf[1:, n])
-    p_h = pdf[1:, n]
-    best = np.log(p_zero/p_h_max)
-    lik = np.log(p_zero/p_h)
+    best = np.log(p_zero / p_h_max)
+    lik = np.log(p_zero / p_h)
     prof_lik = -2 * (best - lik)
     return prof_lik
--- lib/lightcurve.py
+++ lib/lightcurve.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 
+
 class LightCurve(object):
-    def __init__(self, bins: np.array, states: np.array, thr:np.float=0.):
+    def __init__(
+        self,
+        bins: np.array,
+        states: np.array,
+        thr: np.float = 0.0,
+        name: str = "Name",
+    ):
         if len(states) != len(bins) - 1:
             print("Wrong dim!")
             self._bins = None
@@ -12,6 +19,7 @@ class LightCurve(object):
             self._states = states
 
         self._threshold = thr
+        self._name = name
 
         length = np.abs(np.max(self._bins) - np.min(self._bins))
         flare_length = 0.0
@@ -26,11 +34,29 @@ class LightCurve(object):
             print("t outside range")
             return None
 
-        index = np.array((np.sum(self._bins <= t[:, np.newaxis], axis=1) - 1), dtype=np.int)
-        index[index>=len(self._states)] = len(self._states)-1
+        index = np.array(
+            (np.sum(self._bins <= t[:, np.newaxis], axis=1) - 1), dtype=np.int
+        )
+        index[index >= len(self._states)] = len(self._states) - 1
 
-        return [self._states[i]>self._threshold for i in index]
+        return [self._states[i] > self._threshold for i in index]
 
     @property
     def flare_probability(self):
         return self._flare_probability
+
+    @property
+    def bins(self):
+        return self._bins
+
+    @property
+    def states(self):
+        return self._states
+
+    @property
+    def threshold(self):
+        return self._threshold
+
+    @property
+    def name(self):
+        return self._name
